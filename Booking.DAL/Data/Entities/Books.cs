5fc542c01bded09155709da3082ffaf99879f38b
DartGys <bodan20043@gmail.com>
2024-05-10 21:35:34 +0300

    Entities

---
+++ Booking.DAL/Data/Entities/Books.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Booking.DAL.Data.Entities
+{
+    public class Books
+    {
+        public Guid UserId { get; set; }
+        public Guid ApartamentId { get; set; }
+        public Apartaments Apartaments { get; set; }
+        public User User { get; set; }
+    }
+}
