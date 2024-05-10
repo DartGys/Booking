e7ac79c25cf08359f74f53cf7ca012e7b437c060
DartGys <bodan20043@gmail.com>
2024-05-10 22:34:37 +0300

    Services Interfaces

---
+++ Booking.BLL/Interfaces/IBookService.cs
@@ -0,0 +1,13 @@
+﻿using Booking.BLL.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Booking.BLL.Interfaces
+{
+    public interface IBookService : ICrud<BookModel>
+    {
+    }
+}
