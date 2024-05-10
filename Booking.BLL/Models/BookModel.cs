69664d116c5db67f9538c7141711171364f8aad2
DartGys <bodan20043@gmail.com>
2024-05-10 22:23:04 +0300

    Models

---
+++ Booking.BLL/Models/BookModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Booking.BLL.Models
+{
+    public class BookModel
+    {
+        public Guid Id { get; set; }
+        public Guid UserId { get; set; }
+        public Guid ApartamentId { get; set; }
+        public DateTime StartDate { get; set; }
+        public DateTime EndDate { get; set; }
+        public decimal Price { get; set; }
+    }
+}
