be057213e8ce1797054465a4b139eb5a754eda48
DartGys <bodan20043@gmail.com>
2024-05-10 22:04:10 +0300

    configurations

---
+++ Booking.DAL/Data/Configurations/BookConfigurations.cs
@@ -0,0 +1,36 @@
+﻿using Booking.DAL.Data.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Booking.DAL.Data.Configurations
+{
+    public class BookConfigurations : IEntityTypeConfiguration<Book>
+    {
+        public void Configure(EntityTypeBuilder<Book> builder)
+        {
+            builder.HasKey(x => x.Id);
+
+            builder.Property(x => x.StartDate)
+                .IsRequired();
+
+            builder.Property(x => x.EndDate)
+                .IsRequired();
+
+            builder.Property(x => x.Price)
+                .IsRequired();
+
+            builder.HasOne(x => x.User)
+                .WithMany(u => u.Books)
+                .HasForeignKey(x => x.UserId);
+
+            builder.HasOne(x => x.Apartament)
+                .WithMany(u => u.Books)
+                .HasForeignKey(x => x.ApartamentId);
+        }
+    }
+}
